8a7327cd547540376df1516a2f7ab471ad649e4f
pabloguarneros <wrno.pablo@gmail.com>
2021-07-12 16:40:29 -0500

    Added a static root to work

--- greenbook/settings.py
+++ greenbook/settings.py
@@ -153,6 +153,7 @@ COMPRESS_ROOT = 'static'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
 )
 
 STATICFILES_DIRS = [
