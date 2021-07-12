a75268bedd758074135e82264cf50abe037e0079
pabloguarneros <wrno.pablo@gmail.com>
2021-07-12 16:48:10 -0500

    Updated requirements file

--- greenbook/settings.py
+++ greenbook/settings.py
@@ -2,6 +2,8 @@
 from pathlib import Path
 from decouple import config
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
