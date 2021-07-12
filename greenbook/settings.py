27cc2abe36c4e10fe8c4c6b1d1e4f7abf3d03d8b
pabloguarneros <wrno.pablo@gmail.com>
2021-07-12 17:12:49 -0500

    Added django extensions to allow sessions

--- greenbook/settings.py
+++ greenbook/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'django_extensions',
     'rest_framework',
     'api',
     'camera',
